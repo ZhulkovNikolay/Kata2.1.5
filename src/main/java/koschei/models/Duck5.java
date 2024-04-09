325d399da125cbdf7d457c827d776cdefd0ff841
ZhulkovNikolay <wed10@yandex.ru>
2024-04-09 17:17:13 +0300

    full sentence

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+//Метод связывания через поле (добвлен так же Бин в AppConfig )
 @Component
 public class Duck5 {
 
+    @Autowired
+    private Egg6 egg;
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,16 @@
 package koschei.models;
 
+//Метод связывания через констурктор по аналогии с Island2
 public class Egg6 {
+
+    private Needle7 needle;
+
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
